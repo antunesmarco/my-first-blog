f6c520eba5ca6084eded8bba76574eb58137586e
antunes <jrmarco73@gmail.com>
2017-11-26 14:24:52 -0200

    view e template para o detalhe do país

--- imoveis/urls.py
+++ imoveis/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.real_state_search, name='real_state_search'),
+	url(r'^imoveis/(?P<pk>\d+)/$', views.country_detail, name='country_detail'),
 ]
 
--- imoveis/views.py
+++ imoveis/views.py
@@ -1,11 +1,13 @@
 from django.shortcuts import render
 from .models import Country
+from django.shortcuts import render, get_object_or_404
 
 def real_state_search(request):
-#	for c in Country.objects.all():
-		#countries += c.country + ' ' + c.currency_symbol
 	countries = Country.objects.all()
-
 	return render(request, 'imoveis/real_state_search.html', {'countries':countries})
+
+def country_detail(request, pk):
+	country = get_object_or_404(Country, pk=pk)
+	return render(request, 'imoveis/country_detail.html', {'country':country})
 
 
