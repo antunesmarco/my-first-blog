aff269870aae6a843ead579bcc5f0990ad24f16b
antunes <jrmarco73@gmail.com>
2017-11-26 11:20:58 -0200

    Mostrando países do banco no html

--- imoveis/views.py
+++ imoveis/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
+from .models import Country
 
 def real_state_search(request):
-	return render(request, 'imoveis/real_state_search.html')
+#	for c in Country.objects.all():
+		#countries += c.country + ' ' + c.currency_symbol
+	countries = Country.objects.all()
+
+	return render(request, 'imoveis/real_state_search.html', {'countries':countries})
 
 
